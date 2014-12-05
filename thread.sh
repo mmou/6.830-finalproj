bf1bda516de5315812ba8300f543e7988172940c
shiyangliu <lsyang@mit.edu>
2014-12-05 13:00:32 -0500

    modified work flow

--- thread.sh
+++ thread.sh
@@ -9,18 +9,18 @@ workload=(a b c d) #put more
 #client/thread
 
 #parameters
-threads=(1 10 15)
+threads=(1 5 10)
 threads_opcount=10
 threads_recordcount=10
 threads_output=threads_output_all
 rm -f $threads_output
 
-for workload_num in ${workload[@]}; do
-        echo $workload_num
-        ./bin/ycsb load $database -P $db_prop -threads 10 -p recordcount=$threads_recordcount -P workloads/workload$workload_num -s > workload${workload_num}_load_res.txt
+ ./bin/ycsb load $database -P $db_prop -threads 10 -p recordcount=$threads_recordcount -P workloads/workloada -s > workloada_load_res.txt
 
-        for i in ${threads[@]}; do
-                ./bin/ycsb run $database -P $db_prop -threads $i -p operationcount=$threads_opcount -P workloads/workload$workload_num -s > workload${workload_num}_${i}_run_res.txt
+for i in ${threads[@]}; do
+        for workload_num in ${workload[@]}; do
+                echo $i
+                ./bin/ycsb run $database -P $db_prop -threads $i -p recordcount=$threads_recordcount -p operationcount=$threads_opcount -P workloads/workload$workload_num -s > workload${workload_num}_${i}_run_res.txt
                 printf "Threads \n workload${workload_num}_${i}_run_res.txt \n with parameter workload $workload_num -threads $i -p operationcount=$threads_opcount -P workloads/workload$workload_num \n created on $(date +%Y%m%d)" >> $threads_output
                 grep [overall] workloada_${i}_run_res.txt | grep -v YCSB | grep -v com.yahoo >> $threads_output
         done
--- throughput.sh
+++ throughput.sh
@@ -14,16 +14,15 @@ tl_threads=10
 tl_opcount=10
 tl_recordcount=10
 tl_output=tl_output_all
+
 rm -f $tl_output
+./bin/ycsb load $database -P $db_prop -threads $tl_threads -p recordcount=$tl_recordcount -P workloads/workloada -s > workloada_load_res.txt
 
-for workload_num in ${workload[@]}; do
-        echo $workload_num
-        ./bin/ycsb load $database -P $db_prop -threads $tl_threads -p recordcount=$tl_recordcount -P workloads/workload$workload_num -s > workload${workload_num}_load_res.txt
-
-        for i in ${throughput[@]}; do
-                ./bin/ycsb run $database -P $db_prop -threads $tl_threads -p operationcount=$tl_opcount -P workloads/workload$workload_num -s > workload${workload_num}_${i}_run_res.txt
-                printf "workload${workload_num}_${i}_run_res.txt \n with parameter workload $workload_num -threads $tl_threads -p operationcount=$tl_opcount -P workloads/workload$workload_num \n created on $(date +%Y%m%d)" >> $tl_output
+for i in ${throughput[@]}; do
+        for workload_num in ${workload[@]}; do
+                echo target $i 'workload'  $workload_num
+                ./bin/ycsb run $database -P $db_prop -threads $tl_threads -p recordcount=$tl_recordcount -p operationcount=$tl_opcount -target $i -P workloads/workload$workload_num -s > workload${workload_num}_${i}_run_res.txt
+                printf "workload${workload_num}_${i}_run_res.txt \n with parameter workload $workload_num -threads $tl_threads -p operationcount=$tl_opcount -P workloads/workload$workload_num \n created on $(date +%Y%m%d)\n" >> $tl_output
                 grep [overall] workloada_${i}_run_res.txt | grep -v YCSB | grep -v com.yahoo >> $tl_output
-
         done
 done
